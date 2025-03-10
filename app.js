a82cb21ce74e37617101a87a235abdf1d661ebc3
DrMurphyUS <medicosequeiros@gmail.com>
2025-03-10 13:13:11 -0500

    Contador de Intentos

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 //Variables
 let numeroSecreto = 4;
 let numeroUsuario = 0;
+let intentos = 1;
+let palabraVeces = 'vez';
+
+
+
 while(numeroUsuario != numeroSecreto){
     numeroUsuario = prompt("Indicame un numero entre 1 y 10:");
 
@@ -12,7 +17,7 @@ while(numeroUsuario != numeroSecreto){
     */
     if (numeroUsuario == numeroSecreto) {
         //acertamos fue verdadera la condición
-        alert(`Adivinaste el número. El número es: ${numeroUsuario}`);
+        alert(`Adivinaste el número. El número es: ${numeroUsuario} , lo hiciste en ${intentos} ${palabraVeces}`);
     }
     else
     {
@@ -22,5 +27,8 @@ while(numeroUsuario != numeroSecreto){
         }else{
             alert("El numero secreto es mayor");
         }
+        //incrementando cuando el contador no acierta
+        intentos = intentos + 1;
+        palabraVeces ='veces';
     }
 }
