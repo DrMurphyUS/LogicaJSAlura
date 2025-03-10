09d4c6ee94afae1e2e924de62ecfcca7c418a262
DrMurphyUS <medicosequeiros@gmail.com>
2025-03-10 12:55:59 -0500

    Loops y Bucles

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 //Variables
 let numeroSecreto = 4;
-let numeroUsuario = prompt("Indicame un numero entre 1 y 10:");
+let numeroUsuario = 0;
+while(numeroUsuario != numeroSecreto){
+    numeroUsuario = prompt("Indicame un numero entre 1 y 10:");
 
-console.log(numeroUsuario);
+    console.log(numeroUsuario);
 
-/*
-Este código realiza
-la comparación
-*/
-if (numeroUsuario == numeroSecreto) {
-    //acertamos fue verdadera la condición
-    alert(`Adivinaste el número. El número es: ${numeroUsuario}`);
-}
-else
-{
-    if (numeroUsuario > numeroSecreto){
-    //numero equivocado presiona F5 para intentar de nuevo
-    alert("El número secreto es menor");
-    }else{
-        alert("El numero secreto es mayor");
+    /*
+    Este código realiza
+    la comparación
+    */
+    if (numeroUsuario == numeroSecreto) {
+        //acertamos fue verdadera la condición
+        alert(`Adivinaste el número. El número es: ${numeroUsuario}`);
+    }
+    else
+    {
+        if (numeroUsuario > numeroSecreto){
+        //numero equivocado presiona F5 para intentar de nuevo
+        alert("El número secreto es menor");
+        }else{
+            alert("El numero secreto es mayor");
+        }
     }
 }
