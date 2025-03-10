29c05823095c5da5fe1ccaac0f739bb1ff811020
DrMurphyUS <medicosequeiros@gmail.com>
2025-03-10 10:18:02 -0500

    Conecto JS con html

---
+++ app.js
@@ -0,0 +1,8 @@
+let numeroSecreto = 6;
+let numeroUsuario = prompt("Adivina el numero:");
+
+console.log(numeroUsuario);
+
+if (numeroUsuario == numeroSecreto) {
+    alert('Adivinaste el numero');
+}
