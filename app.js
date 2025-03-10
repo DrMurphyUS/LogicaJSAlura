83f1b6f1ab6a54d999d69713f63603470330b359
DrMurphyUS <medicosequeiros@gmail.com>
2025-03-10 11:44:10 -0500

    Comentarios y Else

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+//Variables
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Adivina el numero:");
+let numeroUsuario = prompt("Indicame un numero entre 1 y 10:");
 
 console.log(numeroUsuario);
 
+/*
+Este código realiza
+la comparación
+*/
 if (numeroUsuario == numeroSecreto) {
+    //acertamos fue verdadera la condición
     alert('Adivinaste el numero');
 }
+else
+{
+    //numero equivocado presiona F5 para intentar de nuevo
+    alert("Lo siento te equivocaste")}
